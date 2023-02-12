33b43004c7ddb12d9e3430705d8756affba26027
MrCsabaToth <csaba.toth.us@gmail.com>
2023-02-11 18:39:33 -0800

    This is bad, the recycling kotlin extension is no more, but could not get the new view binding to work, so need to findViewById. Needs ot be fixed because it's ugly

--- app/src/main/java/dev/csaba/diygpstracker/ui/adapter/AssetAdapter.kt
+++ app/src/main/java/dev/csaba/diygpstracker/ui/adapter/AssetAdapter.kt
@@ -3,13 +3,16 @@ package dev.csaba.diygpstracker.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import dev.csaba.diygpstracker.R
 import dev.csaba.diygpstracker.data.Asset
 import dev.csaba.diygpstracker.data.remote.mapPeriodIntervalToProgress
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.recycler_item.view.*
 
 
 class AssetAdapter(private val assetInputListener: OnAssetInputListener?) : RecyclerView.Adapter<AssetViewHolder>() {
@@ -21,8 +24,9 @@ class AssetAdapter(private val assetInputListener: OnAssetInputListener?) : Recy
         inputListener = assetInputListener
         val view = LayoutInflater.from(parent.context).inflate(
             R.layout.recycler_item, parent, false
-        )
-        view.trackAsset.setOnClickListener {
+        ) as ConstraintLayout
+        val trackAsset = view.findViewById<MaterialButton>(R.id.trackAsset)
+        trackAsset.setOnClickListener {
             button -> assetInputListener?.run {
                 this.onTrackClick(button.tag as String)
             }
@@ -35,19 +39,29 @@ class AssetAdapter(private val assetInputListener: OnAssetInputListener?) : Recy
     override fun onBindViewHolder(holder: AssetViewHolder, position: Int) {
         val asset = assetList[position]
         with(holder.containerView) {
+            val assetTitle = findViewById<TextView>(R.id.assetTitle)
             assetTitle.text = asset.title
+            val assetLockLat = findViewById<TextView>(R.id.assetLockLat)
             assetLockLat.text = "${asset.lockLat}"
+            val assetLockLon = findViewById<TextView>(R.id.assetLockLon)
             assetLockLon.text = "${asset.lockLon}"
+            val assetLockRadius = findViewById<TextView>(R.id.assetLockRadius)
             assetLockRadius.text = "${asset.lockRadius}"
+            val assetLockRadiusSeekBar = findViewById<SeekBar>(R.id.assetLockRadiusSeekBar)
             assetLockRadiusSeekBar.progress = asset.lockRadius / 25
+            val assetPeriodInterval = findViewById<TextView>(R.id.assetPeriodInterval)
             assetPeriodInterval.text = "${asset.periodInterval}"
+            val assetPeriodIntervalSeekBar = findViewById<SeekBar>(R.id.assetPeriodIntervalSeekBar)
             assetPeriodIntervalSeekBar.progress = mapPeriodIntervalToProgress(asset.periodInterval)
 
             val assetTag = assetList[position].id
+            val trackAsset = findViewById<MaterialButton>(R.id.trackAsset)
             trackAsset.tag = assetTag
+            val flipAssetLock = findViewById<MaterialButton>(R.id.flipAssetLock)
             flipAssetLock.setIconResource(
                 if (asset.lock) R.drawable.ic_lock_closed else R.drawable.ic_lock_open
             )
+            val statusIcon = findViewById<ImageView>(R.id.statusIcon)
             statusIcon.setImageResource(
                 if (asset.lockAlert) R.drawable.ic_warning else R.drawable.ic_check_circle
             )
@@ -64,7 +78,7 @@ class AssetAdapter(private val assetInputListener: OnAssetInputListener?) : Recy
     }
 }
 
-class AssetViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer
+class AssetViewHolder(val containerView: View): RecyclerView.ViewHolder(containerView)
 
 interface OnAssetInputListener {
     fun onTrackClick(assetId: String)
